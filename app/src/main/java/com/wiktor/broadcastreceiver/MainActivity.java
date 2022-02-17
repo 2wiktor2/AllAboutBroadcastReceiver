ae8fb1fe38b4f0a518fd22a210d2c1b1d2b3d0ae
wiktor <wiktor@bk.ru>
2022-02-17 14:13:03 +0300

    1234

--- app/src/main/java/com/wiktor/broadcastreceiver/MainActivity.java
+++ app/src/main/java/com/wiktor/broadcastreceiver/MainActivity.java
@@ -3,6 +3,7 @@ package com.wiktor.broadcastreceiver;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
@@ -15,7 +16,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button button;
     private TextView textView;
 
-    public static final String WHERE_MY_CAT_ACTION = "CAT";
+    private static final String LOG_TAG = "qwertyu";
+    final String CLASS_NAME = "MainActivity";
+
+    public static final String ACTION_TEST = "TEST";
     public static final String ALARM_MESSAGE = "Срочно пришлите кота!";
 
     private MessageReceiver receiver = new MessageReceiver();
@@ -24,6 +28,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(LOG_TAG, CLASS_NAME + "   ---   " + "onCreate");
 
         textView = findViewById(R.id.textViewHelloWorld);
         button = findViewById(R.id.buttonTestSend);
@@ -31,15 +36,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         registerBroadcastReceiver();
 
-        
-        this.getWindow().addFlags(WindowManager.LayoutParams.FLAG_SECURE);
+
+/*        this.getWindow().addFlags(WindowManager.LayoutParams.FLAG_SECURE);
         this.getWindow().addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN);
-        this.getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        this.getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);*/
     }
 
     @Override
     public void onClick(View v) {
-        textView.setText("Click");
+        textView.setText("Test");
 
         sendMessage(v);
 
@@ -47,7 +52,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     public void sendMessage(View view) {
         Intent intent = new Intent();
-        intent.setAction(WHERE_MY_CAT_ACTION);
+        intent.setAction(ACTION_TEST);
         intent.putExtra("ru.alexanderklimov.broadcast.Message", ALARM_MESSAGE);
         intent.addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
         sendBroadcast(intent);
@@ -55,23 +60,48 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
     public void registerBroadcastReceiver() {
+
+        Log.i(LOG_TAG, CLASS_NAME + "   ---   " + "registerBroadcastReceiver");
+
         this.registerReceiver(receiver, new IntentFilter(
-                WHERE_MY_CAT_ACTION));
+                ACTION_TEST));
+        Log.i(LOG_TAG, CLASS_NAME + "   ---   " + ACTION_TEST);
 
         this.registerReceiver(receiver, new IntentFilter(
                 "android.intent.action.AIRPLANE_MODE"));
+        Log.i(LOG_TAG, CLASS_NAME + "   ---   " + "android.intent.action.AIRPLANE_MODE");
 
         this.registerReceiver(receiver, new IntentFilter(
                 "android.intent.action.MEDIA_SCANNER_SCAN_FILE"));
+        Log.i(LOG_TAG, CLASS_NAME + "   ---   " + "android.intent.action.MEDIA_SCANNER_SCAN_FILE");
 
         this.registerReceiver(receiver, new IntentFilter(
                 "android.intent.action.ACTION_NEW_PICTURE"));
+        Log.i(LOG_TAG, CLASS_NAME + "   ---   " + "android.intent.action.ACTION_NEW_PICTURE");
 
         this.registerReceiver(receiver, new IntentFilter(
                 "android.intent.action.ACTION_POWER_DISCONNECTED"));
+        Log.i(LOG_TAG, CLASS_NAME + "   ---   " + "android.intent.action.ACTION_POWER_DISCONNECTED");
 
         this.registerReceiver(receiver, new IntentFilter(
                 "android.intent.action.ACTION_POWER_CONNECTED"));
+        Log.i(LOG_TAG, CLASS_NAME + "   ---   " + "android.intent.action.ACTION_POWER_CONNECTED");
+
+        this.registerReceiver(receiver, new IntentFilter(
+                "miui.intent.TAKE_SCREENSHOT"));
+        Log.i(LOG_TAG, CLASS_NAME + "   ---   " + "miui.intent.TAKE_SCREENSHOT");
+
+        this.registerReceiver(receiver, new IntentFilter(
+                "android.intent.TAKE_SCREENSHOT"));
+        Log.i(LOG_TAG, CLASS_NAME + "   ---   " + "android.intent.TAKE_SCREENSHOT");
+
+        this.registerReceiver(receiver, new IntentFilter(
+                "android.intent.action.BATTERY_CHANGED"));
+        Log.i(LOG_TAG, CLASS_NAME + "   ---   " + "android.intent.action.BATTERY_CHANGED");
+
+        this.registerReceiver(receiver, new IntentFilter(
+                "android.media.VOLUME_CHANGED_ACTION"));
+        Log.i(LOG_TAG, CLASS_NAME + "   ---   " + "android.media.VOLUME_CHANGED_ACTION");
 
     }
 }
--- app/src/main/java/com/wiktor/broadcastreceiver/MessageReceiver.java
+++ app/src/main/java/com/wiktor/broadcastreceiver/MessageReceiver.java
@@ -3,41 +3,48 @@ package com.wiktor.broadcastreceiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.widget.Toast;
 
 public class MessageReceiver extends BroadcastReceiver {
 
+    private static final String LOG_TAG = "qwertyu";
+    final String CLASS_NAME = "MessageReceiver";
+
     @Override
     public void onReceive(Context context, Intent intent) {
+
+        Log.i(LOG_TAG, CLASS_NAME + "   ---   " + "onReceive" + "        --------------------------");
+
         String message = "Обнаружено сообщение "
                 + intent.getAction();
 
-        if (intent.getAction().equalsIgnoreCase("android.intent.action.AIRPLANE_MODE")) {
+        Log.i(LOG_TAG, CLASS_NAME + "   ---   " + "onReceive" + "   ---   " + message);
 
-            Toast.makeText(context.getApplicationContext(), message,
-                    Toast.LENGTH_LONG).show();
+
+        if (intent.getAction().equalsIgnoreCase("android.intent.action.AIRPLANE_MODE")) {
 
         } else if (intent.getAction().equalsIgnoreCase("android.intent.action.MEDIA_SCANNER_SCAN_FILE")) {
 
-            Toast.makeText(context.getApplicationContext(), message,
-                    Toast.LENGTH_LONG).show();
-
-        } else if (intent.getAction().equalsIgnoreCase("cat")) {
+        } else if (intent.getAction().equalsIgnoreCase("TEST")) {
 
             Toast.makeText(context.getApplicationContext(), message,
                     Toast.LENGTH_LONG).show();
+            Log.i(LOG_TAG, CLASS_NAME + "   ---   " + "                                           onReceive " + message);
         } else if (intent.getAction().equalsIgnoreCase("android.intent.action.ACTION_NEW_PICTURE")) {
 
-            Toast.makeText(context.getApplicationContext(), message,
-                    Toast.LENGTH_LONG).show();
         } else if (intent.getAction().equalsIgnoreCase("android.intent.action.ACTION_POWER_DISCONNECTED")) {
 
-            Toast.makeText(context.getApplicationContext(), message,
-                    Toast.LENGTH_LONG).show();
         } else if (intent.getAction().equalsIgnoreCase("android.intent.action.ACTION_POWER_CONNECTED")) {
 
-            Toast.makeText(context.getApplicationContext(), message,
-                    Toast.LENGTH_LONG).show();
+        } else if (intent.getAction().equalsIgnoreCase("miui.intent.TAKE_SCREENSHOT")) {
+
+        } else if (intent.getAction().equalsIgnoreCase("android.intent.TAKE_SCREENSHOT")) {
+
+        } else if (intent.getAction().equalsIgnoreCase("android.intent.action.BATTERY_CHANGED")) {
+
+        } else if (intent.getAction().equalsIgnoreCase("android.media.VOLUME_CHANGED_ACTION")) {
+
         }
     }
 }
