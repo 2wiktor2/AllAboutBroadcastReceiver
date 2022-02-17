ebcc127f79b58a87b64efc47772810533b9fe4f9
wiktor <wiktor@bk.ru>
2022-02-17 18:47:23 +0300

    12345678

--- app/src/main/java/com/wiktor/broadcastreceiver/MainActivity.java
+++ app/src/main/java/com/wiktor/broadcastreceiver/MainActivity.java
@@ -10,7 +10,6 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -21,6 +20,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private Button button;
     private TextView textView;
+    private TextView textView2;
 
     private static final String LOG_TAG = "qwertyu";
     final String CLASS_NAME = "MainActivity";
@@ -31,38 +31,68 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private BroadcastReceiver receiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            Log.i("qwertyu", "Received: " + intent.getAction() + " " + context.toString());
 
+            Log.i(LOG_TAG, CLASS_NAME + "   ---   " + "onReceive" + "        --------------------------");
 
             String message = "Обнаружено сообщение "
                     + intent.getAction();
 
-            if (intent.getAction().equalsIgnoreCase("android.intent.action.AIRPLANE_MODE")) {
+            Log.i(LOG_TAG, CLASS_NAME + "   ---   " + "onReceive" + "   ---   " + message);
 
-                Toast.makeText(context.getApplicationContext(), message,
-                        Toast.LENGTH_LONG).show();
+            if (intent.getAction().equalsIgnoreCase("TEST")) {
+                Log.i(LOG_TAG, CLASS_NAME + "   ---   " + " onReceive " + "TEST");
+                textView2.setText("\"TEST\"");
+                //  Toast.makeText(context.getApplicationContext(), message, Toast.LENGTH_LONG).show();
+            } else if (intent.getAction().equalsIgnoreCase("android.intent.action.AIRPLANE_MODE")) {
+
+                Log.i(LOG_TAG, CLASS_NAME + "   ---   " + " onReceive " + "AIRPLANE_MODE");
+                textView2.setText("\"AIRPLANE_MODE\"");
 
             } else if (intent.getAction().equalsIgnoreCase("android.intent.action.MEDIA_SCANNER_SCAN_FILE")) {
 
-                Toast.makeText(context.getApplicationContext(), message,
-                        Toast.LENGTH_LONG).show();
+                Log.i(LOG_TAG, CLASS_NAME + "   ---   " + " onReceive " + "MEDIA_SCANNER_SCAN_FILE");
+                textView2.setText("\"MEDIA_SCANNER_SCAN_FILE\"");
 
-            } else if (intent.getAction().equalsIgnoreCase("cat")) {
-
-                Toast.makeText(context.getApplicationContext(), message,
-                        Toast.LENGTH_LONG).show();
             } else if (intent.getAction().equalsIgnoreCase("android.intent.action.ACTION_NEW_PICTURE")) {
 
-                Toast.makeText(context.getApplicationContext(), message,
-                        Toast.LENGTH_LONG).show();
+                Log.i(LOG_TAG, CLASS_NAME + "   ---   " + " onReceive " + "ACTION_NEW_PICTURE");
+                textView2.setText("\"ACTION_NEW_PICTURE\"");
+
             } else if (intent.getAction().equalsIgnoreCase("android.intent.action.ACTION_POWER_DISCONNECTED")) {
 
-                Toast.makeText(context.getApplicationContext(), message,
-                        Toast.LENGTH_LONG).show();
+                Log.i(LOG_TAG, CLASS_NAME + "   ---   " + " onReceive " + "ACTION_POWER_DISCONNECTED");
+                textView2.setText("\"ACTION_POWER_DISCONNECTED\"");
+
             } else if (intent.getAction().equalsIgnoreCase("android.intent.action.ACTION_POWER_CONNECTED")) {
 
-                Toast.makeText(context.getApplicationContext(), message,
-                        Toast.LENGTH_LONG).show();
+                Log.i(LOG_TAG, CLASS_NAME + "   ---   " + " onReceive " + "ACTION_POWER_CONNECTED");
+                textView2.setText("\"ACTION_POWER_CONNECTED\"");
+
+            } else if (intent.getAction().equalsIgnoreCase("miui.intent.TAKE_SCREENSHOT")) {
+
+                Log.i(LOG_TAG, CLASS_NAME + "   ---   " + " onReceive " + "TAKE_SCREENSHOT");
+                textView2.setText("\"TAKE_SCREENSHOT\"");
+
+            } else if (intent.getAction().equalsIgnoreCase("android.window.TAKE_SCREENSHOT")) {
+
+                Log.i(LOG_TAG, CLASS_NAME + "   ---   " + " onReceive " + "TAKE_SCREENSHOT222");
+                textView2.setText("\"TAKE_SCREENSHOT222\"");
+
+            } else if (intent.getAction().equalsIgnoreCase("android.intent.action.BATTERY_CHANGED")) {
+
+                Log.i(LOG_TAG, CLASS_NAME + "   ---   " + " onReceive " + "BATTERY_CHANGED");
+                textView2.setText("\"BATTERY_CHANGED\"");
+
+            } else if (intent.getAction().equalsIgnoreCase("android.media.VOLUME_CHANGED_ACTION")) {
+
+                Log.i(LOG_TAG, CLASS_NAME + "   ---   " + " onReceive " + "VOLUME_CHANGED_ACTION");
+                textView2.setText("\"VOLUME_CHANGED_ACTION\"");
+
+            } else if (intent.getAction().equalsIgnoreCase("android.media.ACTION_MEDIA_SCANNER_SCAN_FILE")) {
+
+                Log.i(LOG_TAG, CLASS_NAME + "   ---   " + " onReceive " + "ACTION_MEDIA_SCANNER_SCAN_FILE");
+                textView2.setText("\"ACTION_MEDIA_SCANNER_SCAN_FILE\"");
+
             }
         }
 
@@ -74,15 +104,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         textView = findViewById(R.id.textViewHelloWorld);
+        textView2 = findViewById(R.id.textViewHelloWorld2);
         button = findViewById(R.id.buttonTestSend);
         button.setOnClickListener(this);
 
         registerBroadcastReceiver();
 
-
-        this.getWindow().addFlags(WindowManager.LayoutParams.FLAG_SECURE);
-        this.getWindow().addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN);
-        this.getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        //Установка флага на запрет скриншота
+        setFlags(false);
     }
 
     @Override
@@ -90,26 +119,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         textView.setText("Click");
 
         sendMessage(v);
-
-
-        Intent intent = new Intent();
-        for (Field field : intent.getClass().getDeclaredFields()) {
-            int modifiers = field.getModifiers();
-            if (Modifier.isPublic(modifiers) &&
-                    Modifier.isStatic(modifiers) &&
-                    Modifier.isFinal(modifiers) &&
-                    field.getType().equals(String.class)) {
-
-                String filter = null;
-                try {
-                    filter = (String) field.get(intent);
-                } catch (IllegalAccessException e) {
-                    e.printStackTrace();
-                }
-                Log.i("qwertyu", "Registered: " + filter);
-                this.registerReceiver(new MessageReceiver(receiver), new IntentFilter(filter));
-            }
-        }
 
     }
 
@@ -166,5 +175,38 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 "android.media.VOLUME_CHANGED_ACTION"));
         Log.i(LOG_TAG, CLASS_NAME + "   ---   " + "android.media.VOLUME_CHANGED_ACTION");
 
+    }
+
+    private void registerAllReceivers() {
+        Intent intent = new Intent();
+        for (Field field : intent.getClass().getDeclaredFields()) {
+            int modifiers = field.getModifiers();
+            if (Modifier.isPublic(modifiers) &&
+                    Modifier.isStatic(modifiers) &&
+                    Modifier.isFinal(modifiers) &&
+                    field.getType().equals(String.class)) {
+
+                String filter = null;
+                try {
+                    filter = (String) field.get(intent);
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+                Log.i("qwertyu", "Registered: " + filter);
+                this.registerReceiver(new MessageReceiver(receiver), new IntentFilter(filter));
+            }
+        }
+    }
+
+    private void setFlags(Boolean on) {
+        if (on) {
+            this.getWindow().addFlags(WindowManager.LayoutParams.FLAG_SECURE);
+            this.getWindow().addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN);
+            this.getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        } else {
+            this.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_SECURE);
+            this.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN);
+            this.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        }
     }
 }
